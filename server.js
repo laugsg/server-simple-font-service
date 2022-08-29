435be84a23aba0b78b2a3396a5fce5edcf2f1e44
laugsg <lautaro.grucci@joingsg.com>
2022-08-29 10:57:16 +0200

    update > edit server for Heroku

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const bodyParser = require('body-parser')
 const express = require('express');
-const path = require('path')
 const app = express();
 
 app.use( bodyParser.urlencoded({extended:false}) )
 app.use( bodyParser.json() )
 
 
+const path = require('path')
 const fontPath = path.join(__dirname, 'public','fonts', 'finalLibraryFont.ttf')
-app.use(express.static(fontPath))
-// console.log("path.join(__dirname, 'public')", fontPath)
+app.use('/finalLibraryFont', app.use(express.static(fontPath)))
 
 
 app.get('/', (req,res) => res.send("server"))
-app.use('/finalLibraryFont', (req,res) => res.sendFile(fontPath))
 
-const port = "3001"
+const port = process.env.PORT || 80
+
 async function appInit( app, port ){
   try {
     await app.listen(
